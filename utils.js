115ec78beb364b433972388abf88aaedbf8610ba
BORSAcoincisiX <140870095+BORSAcoincisiX@users.noreply.github.com>
2025-10-30 21:43:48 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:43:50.178Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-30T18:43:51.724Z
+Commit: Patch security vulnerability in authentication
